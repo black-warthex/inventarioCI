ad02ea0ab0c0b86875b4fb60dad7420856934d6c
Yonathan Montilla <ymontill@bancolombia.com.co>
2023-06-26 23:11:24 -0500

    a

--- src/main/java/com/crud_ci/ci/repository/IInventoryRepository.java
+++ src/main/java/com/crud_ci/ci/repository/IInventoryRepository.java
@@ -3,6 +3,10 @@ package com.crud_ci.ci.repository;
 import com.crud_ci.ci.entity.InventoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IInventoryRepository extends JpaRepository<InventoryEntity, String> {
+    List<InventoryEntity> findAll();
+    List<InventoryEntity> findByinventoryItemName(String name);
 
 }
--- src/main/java/com/crud_ci/ci/service/IInventoryService.java
+++ src/main/java/com/crud_ci/ci/service/IInventoryService.java
@@ -4,7 +4,14 @@ import com.crud_ci.ci.dto.ResponseDto;
 import com.crud_ci.ci.entity.InventoryEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface IInventoryService {
     public ResponseDto addInventory(InventoryEntity inventory);
+
+    public List<InventoryEntity> getAllInventory();
+    public List<InventoryEntity> findByItemName(String name);
+    public ResponseDto deleteInventory(String name);
+    public ResponseDto updateInventoryPricer(String name, Double price);
 }
