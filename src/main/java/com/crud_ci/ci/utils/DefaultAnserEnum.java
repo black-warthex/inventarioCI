2ebcd3ea854eaaa9eb16db4448570e306a8c9f0f
Yonathan Montilla <ymontill@bancolombia.com.co>
2023-06-26 22:59:06 -0500

    final v0.0.1

--- src/main/java/com/crud_ci/ci/utils/DefaultAnserEnum.java
+++ src/main/java/com/crud_ci/ci/utils/DefaultAnserEnum.java
@@ -3,7 +3,7 @@ package com.crud_ci.ci.utils;
 import lombok.Getter;
 
 public enum DefaultAnserEnum {
-    MESSAGE_INSERT("successfully added"), MESSAGE_ERROR("error process failed"), MESSAGE_DUPLICATE("You cannot add this object, it is already registered");
+    MESSAGE_INSERT("successfully added"),MESSAGE_DELETE("removed item"),MESSAGE_UPDATE("item updated"), MESSAGE_ERROR("error process failed"), MESSAGE_DUPLICATE("You cannot add this object, it is already registered");
     @Getter
     private String value;
 
